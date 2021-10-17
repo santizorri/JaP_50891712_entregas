81b01d939f8b95899c23a24afa6321db029893b8
santizorri <santi.zorrilla3@hotmail.com>
2021-10-17 18:58:26 -0300

    Entrega 5 final

--- js/cart.js
+++ js/cart.js
@@ -1,32 +1,32 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-let purshasedProduct = {};
-//let purshasedProductsNumber = [];
+
+let purshasedProduct = {}; //Creamos el objeto donde obtendremos los datos
 
 document.addEventListener("DOMContentLoaded", function(e){
 
     getJSONData(CART_INFO_URL_2).then(function (resultObj) {
         if (resultObj.status === "ok") {
-            purshasedProduct = resultObj.data;
-            //console.log(purshasedProduct);
+            purshasedProduct = resultObj.data; //Guardamos los datos creados en la variable creada
             showPurProd();
         }
         
     });
 
 });
-
+//Funcion que va a mostrar los productos del carrito
 function showPurProd(){
-    let purProd = '';
-    let i = 0;
+    let purProd = ''; //Variable para guardar y mostrar en el HTML
+    let i = 0; //Variable que usaremos de indice para mas adelante, pudiendo identificar cada obejeto
+
     purshasedProduct.articles.forEach((numero) => {
         purProd += `
         <tr>
             <th scope="row"> ${i + 1} </th>
             <td style="width:15%"><img src=" ${numero.src} "alt="..." class=" img-thumbnail mx-auto d-block "></td>
             <td> ${numero.name} </td>
-            <td name="cost" class="price"> ${numero.unitCost} </td>
+            <td class="price"> ${numero.unitCost} </td>
             <td class='currency'> ${numero.currency} </td>
             <td><input type='number' value=${numero.count} id='cant${i}' min=0 max=10 onchange="subTotal();"></td>
             <td id='res${i}' style="font-weight: bold;"> </td>
@@ -35,25 +35,25 @@ function showPurProd(){
         `
         i++;
     })
-    document.getElementById('purshasedProd').innerHTML = purProd;
-    subTotal();
+    document.getElementById('purshasedProd').innerHTML = purProd; //Mostramos el carrito agregandolo al HTML
+    subTotal(); //Cargamos la funcion que calcula el subtotal y el total
 }
-
+//Funcion que nos calcula los totales
 function subTotal(){
-    let prices = document.getElementsByClassName('price');
-    let count = document.getElementsByTagName('input');
+    let prices = document.getElementsByClassName('price'); //Obtenemos los datos de los precios de cada articulo, se crea una lista
+    let count = document.getElementsByTagName('input'); //Obtenemos la cantidad de cada articulo, se crea una lista
 
-    let total = 0;
+    let total = 0; // Inicializamos la variable total
 
-    for (let i=0; i < prices.length; i++){
-        if(purshasedProduct.articles[i].currency == "USD"){
-        document.getElementById('res'+i).innerHTML = parseFloat(prices[i].innerHTML) * 40 * parseFloat(count[i].value);
-        total += parseFloat(prices[i].innerHTML) * 40 * parseFloat(count[i].value);
+    for (let i=0; i < prices.length; i++){ //Este for recorre 
+        if(purshasedProduct.articles[i].currency == "USD"){ //Este if filtra en funcion de si esta en USD o pesos
+        document.getElementById('res'+i).innerHTML = parseFloat(prices[i].innerHTML) * 40 * parseFloat(count[i].value); //Escribimos los subtotales calculandolos como el precio por la cantidad
+        total += parseFloat(prices[i].innerHTML) * 40 * parseFloat(count[i].value); //Vamos sumando al total
         }else{
-            document.getElementById('res'+i).innerHTML = parseFloat(prices[i].innerHTML) * parseFloat(count[i].value);
-            total += parseFloat(prices[i].innerHTML) * parseFloat(count[i].value);;
+            document.getElementById('res'+i).innerHTML = parseFloat(prices[i].innerHTML) * parseFloat(count[i].value); //Escribimos los subtotales calculandolos como el precio por la cantidad
+            total += parseFloat(prices[i].innerHTML) * parseFloat(count[i].value); //Vamos sumando al total
         }
         
     }
-    document.getElementById('total').innerHTML=total;
+    document.getElementById('total').innerHTML=total; // Mostramos el total en el HTML
 }
